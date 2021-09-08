4c44afa3f0b994fd370c87e2520856b220eb8032
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-09-08 11:52:00 +0300

    Replaced custom test only observers by Reactive

--- test/Kinetic.Tests/ObjectTests.cs
+++ test/Kinetic.Tests/ObjectTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using Xunit;
 
 namespace Kinetic.Tests
@@ -27,7 +28,7 @@ namespace Kinetic.Tests
             var numbers = new List<int>();
 
             using (test.Number.Changed.Subscribe(
-                new Observer<int>(value => numbers.Add(value))))
+                value => numbers.Add(value)))
             {
                 test.Number.Set(1);
                 test.Number.Set(2);
@@ -46,7 +47,7 @@ namespace Kinetic.Tests
             var numbers = new List<int>();
 
             test.Number.Changed.Subscribe(
-                new Observer<int>(value => numbers.Add(value)));
+                value => numbers.Add(value));
 
             using (test.SuppressNotifications())
             {
@@ -65,7 +66,7 @@ namespace Kinetic.Tests
             var numbers = new List<int>();
 
             test.Number.Changed.Subscribe(
-                new Observer<int>(value => numbers.Add(value)));
+                value => numbers.Add(value));
 
             test.Number.Set(0);
             test.Number.Set(1);
@@ -80,30 +81,10 @@ namespace Kinetic.Tests
             private string _text = string.Empty;
 
             public TestObject() =>
-                Number.Changed.Subscribe(new NumberChanged(this));
+                Number.Changed.Subscribe(value => Set(Text, value.ToString()));
 
             public Property<int> Number => Property(ref _number);
             public ReadOnlyProperty<string> Text => Property(ref _text);
-
-            private class NumberChanged : IObserver<int>
-            {
-                public TestObject Owner { get; }
-                public NumberChanged(TestObject owner) => Owner = owner;
-
-                public void OnNext(int value) => Owner.Set(Owner.Text, value.ToString());
-                public void OnError(Exception exception) { }
-                public void OnCompleted() { }
-            }
-        }
-
-        private sealed class Observer<T> : IObserver<T>
-        {
-            public Action<T> Handler { get; }
-            public Observer(Action<T> handler) => Handler = handler;
-
-            public void OnNext(T value) => Handler(value);
-            public void OnError(Exception exception) { }
-            public void OnCompleted() { }
         }
     }
 }

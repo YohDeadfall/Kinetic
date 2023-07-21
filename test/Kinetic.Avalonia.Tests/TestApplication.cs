6c4fd3aa751b98ef5296ac6349e2ed80b2f9f59a
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-07-21 12:39:51 +0300

    Added test application builder

---
+++ test/Kinetic.Avalonia.Tests/TestApplication.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Avalonia.Headless;
+using Avalonia.Themes.Simple;
+using Kinetic.Data.Tests;
+
+[assembly: AvaloniaTestApplication(typeof(TestApplication))]
+
+namespace Kinetic.Data.Tests;
+
+public sealed class TestApplication : Application
+{
+    public TestApplication() => Styles.Add(new SimpleTheme());
+
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<TestApplication>()
+        .UseHeadless(new AvaloniaHeadlessPlatformOptions());
+}

1a146b306da23d2f55541e4421705bc52ec2e7a9
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-01-12 21:24:35 +0300

    Implemented OnNextAsync

--- src/Kinetic/Linq/Awaiter.cs
+++ src/Kinetic/Linq/Awaiter.cs
@@ -4,12 +4,37 @@ using System.Threading.Tasks;
 
 namespace Kinetic.Linq;
 
+interface IAwaiter
+{
+    bool IsCompleted { get; }
+    void OnCompleted(Action continuation);
+
+    void GetResult();
+}
+
 interface IAwaiter<T>
 {
     bool IsCompleted { get; }
     void OnCompleted(Action continuation);
 
     T GetResult();
+}
+
+internal struct AwaiterForTask : IAwaiter
+{
+    private readonly TaskAwaiter _awaiter;
+
+    public AwaiterForTask(TaskAwaiter awaiter) =>
+        _awaiter = awaiter;
+
+    public bool IsCompleted =>
+        _awaiter.IsCompleted;
+
+    public void OnCompleted(Action continuation) =>
+        _awaiter.OnCompleted(continuation);
+
+    public void GetResult() =>
+        _awaiter.GetResult();
 }
 
 internal struct AwaiterForTask<T> : IAwaiter<T>
@@ -26,6 +51,23 @@ internal struct AwaiterForTask<T> : IAwaiter<T>
         _awaiter.OnCompleted(continuation);
 
     public T GetResult() =>
+        _awaiter.GetResult();
+}
+
+internal struct AwaiterForValueTask : IAwaiter
+{
+    private readonly ValueTaskAwaiter _awaiter;
+
+    public AwaiterForValueTask(ValueTaskAwaiter awaiter) =>
+        _awaiter = awaiter;
+
+    public bool IsCompleted =>
+        _awaiter.IsCompleted;
+
+    public void OnCompleted(Action continuation) =>
+        _awaiter.OnCompleted(continuation);
+
+    public void GetResult() =>
         _awaiter.GetResult();
 }
 
@@ -46,10 +88,28 @@ internal struct AwaiterForValueTask<T> : IAwaiter<T>
         _awaiter.GetResult();
 }
 
+interface IAwaiterFactory<TAwaiter, TSource>
+    where TAwaiter : struct, IAwaiter
+{
+    TAwaiter GetAwaiter(TSource value);
+}
+
 interface IAwaiterFactory<TAwaiter, TSource, TResult>
     where TAwaiter : struct, IAwaiter<TResult>
 {
     TAwaiter GetAwaiter(TSource value);
+}
+
+internal readonly struct AwaiterFactoryForTask<TSource> :
+    IAwaiterFactory<AwaiterForTask, TSource>
+{
+    private readonly Func<TSource, Task> _factory;
+
+    public AwaiterFactoryForTask(Func<TSource, Task> factory) =>
+        _factory = factory;
+
+    public AwaiterForTask GetAwaiter(TSource value) =>
+        new(_factory(value).GetAwaiter());
 }
 
 internal readonly struct AwaiterFactoryForTask<TSource, TResult> :
@@ -61,6 +121,18 @@ internal readonly struct AwaiterFactoryForTask<TSource, TResult> :
         _factory = factory;
 
     public AwaiterForTask<TResult> GetAwaiter(TSource value) =>
+        new(_factory(value).GetAwaiter());
+}
+
+internal readonly struct AwaiterFactoryForValueTask<TSource> :
+    IAwaiterFactory<AwaiterForValueTask, TSource>
+{
+    private readonly Func<TSource, ValueTask> _factory;
+
+    public AwaiterFactoryForValueTask(Func<TSource, ValueTask> factory) =>
+        _factory = factory;
+
+    public AwaiterForValueTask GetAwaiter(TSource value) =>
         new(_factory(value).GetAwaiter());
 }
 
---
+++ src/Kinetic/Linq/Observable.OnNextAsync.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Threading.Tasks;
+using Kinetic.Linq.StateMachines;
+
+namespace Kinetic.Linq;
+
+public static partial class Observable
+{
+    public static ObserverBuilder<T> OnNextAsync<T>(this IObservable<T> source, Func<T, Task> onNext) =>
+        source.ToBuilder().OnNextAsync(onNext);
+
+    public static ObserverBuilder<T> OnNextAsync<T>(this IObservable<T> source, Func<T, ValueTask> onNext) =>
+        source.ToBuilder().OnNextAsync(onNext);
+
+    public static ObserverBuilder<T> OnNextAsync<T>(this ObserverBuilder<T> source, Func<T, Task> onNext) =>
+        source.ContinueWith<OnNextAsyncStateMachineFactory<T>, T>(new(onNext));
+
+    public static ObserverBuilder<T> OnNextAsync<T>(this ObserverBuilder<T> source, Func<T, ValueTask> onNext) =>
+        source.ContinueWith<OnNextAsyncStateMachineFactory<T>, T>(new(onNext));
+
+    private readonly struct OnNextAsyncStateMachineFactory<T> : IObserverStateMachineFactory<T, T>
+    {
+        private readonly Delegate _onNext;
+
+        public OnNextAsyncStateMachineFactory(Func<T, Task> onNext) =>
+            _onNext = onNext;
+
+        public OnNextAsyncStateMachineFactory(Func<T, ValueTask> onNext) =>
+            _onNext = onNext;
+
+        public void Create<TContinuation>(in TContinuation continuation, ObserverStateMachine<T> source)
+            where TContinuation : struct, IObserverStateMachine<T>
+        {
+            if (_onNext is Func<T, Task> onNextTask)
+            {
+                source.ContinueWith(new OnNextAsyncStateMachine<TContinuation, T, AwaiterForTask, AwaiterFactoryForTask<T>>(continuation, new(onNextTask)));
+                return;
+            }
+
+            if (_onNext is Func<T, ValueTask> onNextValueTask)
+            {
+                source.ContinueWith(new OnNextAsyncStateMachine<TContinuation, T, AwaiterForValueTask, AwaiterFactoryForValueTask<T>>(continuation, new(onNextValueTask)));
+                return;
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+
+    private struct OnNextAsyncStateMachine<TContinuation, T, TAwaiter, TAwaiterFactory> : IObserverStateMachine<T>
+        where TContinuation : struct, IObserverStateMachine<T>
+        where TAwaiter : struct, IAwaiter
+        where TAwaiterFactory : struct, IAwaiterFactory<TAwaiter, T>
+    {
+        private TContinuation _continuation;
+        private readonly TAwaiterFactory _onNext;
+
+        public OnNextAsyncStateMachine(in TContinuation continuation, TAwaiterFactory onNext)
+        {
+            _continuation = continuation;
+            _onNext = onNext;
+        }
+
+        public ObserverStateMachineBox Box =>
+            _continuation.Box;
+
+        public void Initialize(ObserverStateMachineBox box) =>
+            _continuation.Initialize(box);
+
+        public void Dispose() =>
+            _continuation.Dispose();
+
+        public void OnNext(T value)
+        {
+            var awaiter = default(TAwaiter);
+            try
+            {
+                awaiter = _onNext.GetAwaiter(value);
+            }
+            catch (Exception error)
+            {
+                _continuation.OnError(error);
+                return;
+            }
+
+            if (awaiter.IsCompleted)
+            {
+                ForwardResult(value, awaiter, ref _continuation);
+            }
+            else
+            {
+                var reference = new ObserverStateMachineReference<T, TContinuation>(ref _continuation);
+                var completion = () => ForwardResult(value, awaiter, ref reference.Target);
+
+                awaiter.OnCompleted(completion);
+            }
+        }
+
+        public void OnError(Exception error) =>
+            _continuation.OnError(error);
+
+        public void OnCompleted() =>
+            _continuation.OnCompleted();
+
+        private static void ForwardResult(T value, TAwaiter awaiter, ref TContinuation continuation)
+        {
+            try
+            {
+                awaiter.GetResult();
+            }
+            catch (Exception error)
+            {
+                continuation.OnError(error);
+                return;
+            }
+
+            continuation.OnNext(value);
+        }
+    }
+}

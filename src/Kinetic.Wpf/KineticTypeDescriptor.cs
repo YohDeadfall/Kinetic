7237824093cf30575f6e1018fd41f80f82004946
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-05-31 22:49:11 +0300

    Added WPF support

---
+++ src/Kinetic.Wpf/KineticTypeDescriptor.cs
@@ -0,0 +1,57 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Kinetic.Data;
+
+internal sealed class KineticTypeDescriptor : ICustomTypeDescriptor
+{
+    private readonly ICustomTypeDescriptor _parent;
+
+    public KineticTypeDescriptor(ICustomTypeDescriptor parent) =>
+        _parent = parent;
+
+    public AttributeCollection GetAttributes() =>
+        _parent.GetAttributes();
+
+    public string? GetClassName() =>
+        _parent.GetClassName();
+
+    public string? GetComponentName() =>
+        _parent.GetComponentName();
+
+    public TypeConverter? GetConverter() =>
+        _parent.GetConverter();
+
+    public EventDescriptor? GetDefaultEvent() =>
+        _parent.GetDefaultEvent();
+
+    public PropertyDescriptor? GetDefaultProperty() =>
+        _parent.GetDefaultProperty();
+
+    public object? GetEditor(Type editorBaseType) =>
+        _parent.GetEditor(editorBaseType);
+
+    public EventDescriptorCollection GetEvents() =>
+        _parent.GetEvents();
+
+    public EventDescriptorCollection GetEvents(Attribute[]? attributes) =>
+        _parent.GetEvents(attributes);
+
+    public PropertyDescriptorCollection GetProperties() =>
+        GetProperties(null);
+
+    public PropertyDescriptorCollection GetProperties(Attribute[]? attributes)
+    {
+        var properties = _parent
+            .GetProperties(null)
+            .Cast<PropertyDescriptor>()
+            .Select(property => KineticPropertyDescriptor.TryCreate(property) ?? property)
+            .ToArray();
+
+        return new PropertyDescriptorCollection(properties);
+    }
+
+    public object? GetPropertyOwner(PropertyDescriptor? pd) =>
+        _parent.GetPropertyOwner(pd);
+}
---
+++ test/Kinetic.Wpf.Tests/KineticPropertyAccessorTests.cs
@@ -0,0 +1,82 @@
+using System.ComponentModel;
+using System.Windows.Controls;
+using Xunit;
+
+namespace Kinetic.Data.Tests;
+
+public class KineticPropertyAccessorTests
+{
+    static KineticPropertyAccessorTests() =>
+        TypeDescriptor.AddProvider(new KineticTypeDescriptorProvider(), typeof(ObservableObject));
+
+    [AvaloniaFact]
+    public void BindingToProperty()
+    {
+        var source = new Source();
+        var target = new TextBox { DataContext = source };
+        var binding = new Avalonia.Data.Binding { Path = "Text" };
+
+        target.Bind(TextBox.TextProperty, binding);
+
+        source.Text.Set("foo");
+        Assert.Equal("foo", target.Text);
+
+        source.Text.Set("bar");
+        Assert.Equal("bar", target.Text);
+
+        target.Text = "baz";
+        Assert.Equal("baz", source.Text);
+    }
+
+    [AvaloniaFact]
+    public void BindingToReadOnlyProperty()
+    {
+        var source = new Source();
+        var target = new TextBox { DataContext = source };
+        var binding = new Avalonia.Data.Binding { Path = "TextReadOnly" };
+
+        target.Bind(TextBox.TextProperty, binding);
+
+        source.SetTextReadOnly("foo");
+        Assert.Equal("foo", target.Text);
+
+        source.SetTextReadOnly("bar");
+        Assert.Equal("bar", target.Text);
+
+        target.Text = "baz";
+        Assert.Equal("bar", source.TextReadOnly);
+    }
+
+    [AvaloniaFact]
+    public void BindingToList()
+    {
+        var source = new Source();
+        var target = new ItemsControl { DataContext = source };
+        var window = new Window { Content = target };
+        var binding = new Avalonia.Data.Binding { Path = "Items" };
+
+        window.Show(); ;
+        target.Bind(ItemsControl.ItemsSourceProperty, binding);
+
+        Assert.Equal(source.Items, target.ItemsSource);
+
+        source.Items.Add("foo");
+        source.Items.Add("bar");
+
+        Assert.Equal("foo", ((ContentPresenter?) target.ContainerFromIndex(0))?.Content);
+        Assert.Equal("bar", ((ContentPresenter?) target.ContainerFromIndex(1))?.Content);
+    }
+
+    private sealed class Source : ObservableObject
+    {
+        private string? _text;
+        private string? _textReadOnly;
+
+        public Property<string?> Text => Property(ref _text);
+        public ReadOnlyProperty<string?> TextReadOnly => Property(ref _textReadOnly);
+
+        public ObservableList<string> Items { get; } = new();
+
+        public void SetTextReadOnly(string value) => Set(TextReadOnly, value);
+    }
+}

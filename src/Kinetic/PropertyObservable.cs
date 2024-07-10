6bce0a733794b82a742db404bc249b1239c634ec
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-07-10 13:30:47 +0300

    Moving types to their own files

---
+++ src/Kinetic/PropertyObservable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Kinetic;
+
+internal abstract class PropertyObservable
+{
+    internal readonly ObservableObject Owner;
+    internal readonly PropertyObservable? Next;
+    internal readonly IntPtr Offset;
+
+    internal uint Version;
+
+    protected PropertyObservable(ObservableObject owner, IntPtr offset, PropertyObservable? next) =>
+        (Owner, Offset, Next) = (owner, offset, next);
+
+    public abstract void Changed();
+}
+
+internal sealed class PropertyObservable<T> : PropertyObservable, IObservableInternal<T>
+{
+    private ObservableSubscriptions<T> _subscriptions;
+
+    public PropertyObservable(ObservableObject owner, IntPtr offset, PropertyObservable? next) :
+        base(owner, offset, next)
+    { }
+
+    public override void Changed() =>
+        Changed(Owner.Get<T>(Offset));
+
+    public void Changed(T value) =>
+        _subscriptions.OnNext(value);
+
+    public IDisposable Subscribe(IObserver<T> observer)
+    {
+        observer.OnNext(Owner.Get<T>(Offset));
+        return _subscriptions.Subscribe(this, observer);
+    }
+
+    public void Subscribe(ObservableSubscription<T> subscription) =>
+        _subscriptions.Subscribe(this, subscription);
+
+    public void Unsubscribe(ObservableSubscription<T> subscription) =>
+        _subscriptions.Unsubscribe(subscription);
+}
---
+++ src/Kinetic/ReadOnlyProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Kinetic;
+
+[DebuggerDisplay("Get()")]
+public readonly struct ReadOnlyProperty<T>
+{
+    internal readonly ObservableObject Owner;
+    internal readonly IntPtr Offset;
+
+    internal ReadOnlyProperty(ObservableObject owner, IntPtr offset) =>
+        (Owner, Offset) = (owner, offset);
+
+    public T Get() => Owner.Get<T>(Offset);
+
+    public IObservable<T> Changed => Owner.EnsureObservableFor<T>(Offset);
+
+    public static implicit operator T(ReadOnlyProperty<T> property) =>
+        property.Get();
+}

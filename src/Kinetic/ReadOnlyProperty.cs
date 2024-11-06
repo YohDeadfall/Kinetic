f3f8d8af29d0d0eaf4e53b4eb0957a2e2772b8c3
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-11-06 23:14:34 +0300

    Docs for observable properties

--- src/Kinetic/ReadOnlyProperty.cs
+++ src/Kinetic/ReadOnlyProperty.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 
 namespace Kinetic;
 
+/// <summary>
+/// A read-only property of an observable object.
+/// </summary>
 [DebuggerDisplay("{Get()}")]
 [DebuggerTypeProxy(typeof(ReadOnlyPropertyDebugView<>))]
 public readonly struct ReadOnlyProperty<T>
@@ -13,12 +16,27 @@ public readonly struct ReadOnlyProperty<T>
     internal ReadOnlyProperty(ObservableObject owner, IntPtr offset) =>
         (Owner, Offset) = (owner, offset);
 
+    /// <summary>
+    /// Gets a value of this property.
+    /// </summary>
+    /// <returns>A value of this property.</returns>
     public T Get() =>
         Owner.Get<T>(Offset);
 
+    /// <summary>
+    /// Gets an <see cref="IObservable{T}"/> notifying about changes of this property.
+    /// </summary>
+    /// <returns>
+    /// An <see cref="IObservable{T}"/> notifying about changes of this property.
+    /// </returns>
     public IObservable<T> Changed =>
         Owner.EnsureObservableFor<T>(Offset);
 
+    /// <summary>
+    /// Defines an implicit cast of a <see cref="Property{T}"/> to its value.
+    /// </summary>
+    /// <param name="property">The property which value to be returned.</param>
+    /// <returns>A value of the property.</returns>
     public static implicit operator T(ReadOnlyProperty<T> property) =>
         property.Get();
 }

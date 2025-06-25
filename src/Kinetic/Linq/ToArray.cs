bdf8c93bf5ba5573ba555185cde4b3d9a46468c2
Yoh Deadfall <yoh.deadfall@hotmail.com>
2025-06-26 00:18:38 +0300

    ToArray fix

---
+++ src/Kinetic/Linq/ToArray.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using Kinetic.Runtime;
+
+namespace Kinetic.Linq;
+
+public readonly struct ToArray<TOperator, TSource> : IOperator<TSource[]>
+    where TOperator : IOperator<TSource>
+{
+    private readonly TOperator _source;
+
+    public ToArray(TOperator source) =>
+        _source = source.ThrowIfArgumentNull();
+
+    public TBox Build<TBox, TBoxFactory, TContinuation>(in TBoxFactory boxFactory, TContinuation continuation)
+        where TBoxFactory : struct, IStateMachineBoxFactory<TBox>
+        where TContinuation : struct, IStateMachine<TSource[]>
+    {
+        return _source.Build<
+            TBox,
+            TBoxFactory,
+            AggregateStateMachine<
+                TransformStateMachine<
+                    TContinuation,
+                    FuncTransform<List<TSource>, TSource[]>,
+                    List<TSource>,
+                    TSource[]>,
+                CollectIntoAccumulator<TSource, List<TSource>>,
+                TSource,
+                List<TSource>>>(
+            boxFactory, new(new(continuation, new(list => list.ToArray())), accumulator: new(new())));
+    }
+}

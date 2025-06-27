6c4ccd5937afa8efbdd7ab42bf0ed43821772cb3
Yoh Deadfall <yoh.deadfall@hotmail.com>
2025-06-27 06:54:15 +0300

    Fix item ordering comparer

--- src/Kinetic/Linq/OrderItemsByStateComparer.cs
+++ src/Kinetic/Linq/OrderItemsByStateComparer.cs
@@ -3,13 +3,19 @@ using System.Diagnostics;
 
 namespace Kinetic.Linq;
 
-internal sealed class OrderItemsByStateComparer<TState, TKey>(IComparer<TKey> Inner) : IComparer<TState>
+internal sealed class OrderItemsByStateComparer<TState, TKey>(IComparer<TKey>? inner) : IComparer<TState>
     where TState : IOrderItemsByState<TKey>
 {
+    private readonly IComparer<TKey>? _inner = typeof(TKey).IsValueType
+        ? inner is { } && inner == Comparer<TKey>.Default ? null : inner
+        : inner ?? Comparer<TKey>.Default;
+
     public int Compare(TState? x, TState? y)
     {
         Debug.Assert(x is { });
         Debug.Assert(y is { });
-        return Inner.Compare(x.Key, y.Key);
+        return typeof(TKey).IsValueType && _inner is null
+            ? Comparer<TKey>.Default.Compare(x.Key, y.Key)
+            : _inner!.Compare(x.Key, y.Key);
     }
 }
